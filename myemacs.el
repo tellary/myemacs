8631e1b8be06b976a5dc37d57de15f618c71d564
Ilya Silvestrov <isilvestrov@box.com>
2018-03-13 14:40:46 -0700

    Use `melpa-stable`
    Instead of `melpa.milkbox.net`

--- myemacs.el
+++ myemacs.el
@@ -43,7 +43,8 @@
 
 ;; http://ergoemacs.org/emacs/emacs_package_system.html
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
