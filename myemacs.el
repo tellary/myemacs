d9877089f4a3ad642c3f780231413de58fa41544
Ilya Silvestrov <tellary@gmail.com>
2020-01-17 11:53:16 -0800

    Load `tasktags`

--- myemacs.el
+++ myemacs.el
@@ -354,3 +354,6 @@
 ;; Using a deprecated one as it supports Emacs 24
 (load "puml-mode")
 (add-to-list 'auto-mode-alist '("\\.pu$" . puml-mode))
+
+(load "tasktags/stream.el")
+(load "tasktags/task-tags-mode.el")
